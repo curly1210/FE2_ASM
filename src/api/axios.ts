db30fecc5477d4502a223617507f4131b0cfe78b
curly1210 <1210.curly@gmail.com>
2025-04-08 10:18:03 +0700

    fix: baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const config = axios.create({
-  baseURL: "http://localhost:10000",
+  // baseURL: "http://localhost:10000",
+  baseURL: "https://fake-api-vott.onrender.com/",
 });
